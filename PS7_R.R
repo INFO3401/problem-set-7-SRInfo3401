63bd4d168fe58b9f494c99c250cb2e2dd14f2fa9
SRInfo3401 <42791194+SRInfo3401@users.noreply.github.com>
2018-10-21 20:01:14 -0600

    Answers from M/W/F Problems
    Finished: Wednesday and Friday problems, Monday still work in progress.

--- PS7_R.R
+++ PS7_R.R
@@ -45,12 +45,33 @@ table(titanic_data$Sex,  titanic_data$Survived)
 #allows us to look at the percentages a function of either the rows (1) or columns (2)
 prop.table(table(titanic_data$Sex,  titanic_data$Survived), 1or2)
 
-#8. Compute the proportion of men and women who survived. Then, compute the probability that if someone was a women, they would survive the crash and the probability that if someone was a man, they would survive the crash. 
+#8. Compute the proportion of men and women who survived. Then, compute the probability that if someone was a women, they would survive the crash and the probability that if someone was a man, they would survive the crash.
+
 #Compute the proportion of men and women who survived.
 prop.table(table(titanic_data$Sex,  titanic_data$Survived))
 #Compute the probability that if someone was a women, they would survive the crash.
 prop.table(table(titanic_data$Sex,  titanic_data$Survived),1)
 #Compute the probability that if someone was a man, they would survive the crash.
-prop.table(table(titanic_data$Sex,  titanic_data$Survived), 2)
+prop.table(table(titanic_data$Sex,  titanic_data$Survived),2)
+
+#9. We want to test whether the old naval cliche of "women and children first" impacted survival. Assume that a child is anyone under the age of 18.(0-17=1)(18-up=0) Use the titanic dataset to answer this question by computing the average number of survivors for men and women and for children and adults. As a hint, you will need to create a new column using Age to define a child (1 for a child, 0 for not a child).
 #We can define a new column in R that is computed from the available data. One way to do this is to create a new variable that contains only zeros: 
-<variable name>$<column name><-0
+titanic_data$age<-0
+
+#the conditional we want to use goes in square brackets and the values that we assign go after the <-.
+#If age less than 18 give value 1=child
+titanic_data$age[titanic_data$age < 18] <- 1
+
+#Dependent Variable = Survived
+#Independent Variable = male, female
+aggregate(Survived ~ Sex, data=titanic_data, FUN=mean)
+
+#Compute the average number of survivors for men and women
+aggregate(Survived ~ Sex, data=titanic_data, FUN=mean)
+
+#Compute the average number of survivors for children and adults.
+#Age 0 to 1 test code - aggregate(Survived ~ Sex + Age + age, data=titanic_data, FUN=mean)
+titanic_data$age<-0
+titanic_data$age[titanic_data$Age < 18] <- 1
+
+aggregate(Survived ~ age, data=titanic_data, FUN=mean)
